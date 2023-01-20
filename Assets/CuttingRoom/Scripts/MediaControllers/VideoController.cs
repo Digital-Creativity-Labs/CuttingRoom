16922fa9795596da804e4e2d33e91ccb46b9b2f8
Daniel Slawson <daniel.slawson@york.ac.uk>
2023-01-20 14:49:06 +0000

    Add support for non-fullscreen videos using a visual element and render texture.

--- Assets/CuttingRoom/Scripts/MediaControllers/VideoController.cs
+++ Assets/CuttingRoom/Scripts/MediaControllers/VideoController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Video;
+using UnityEngine.UIElements;
+using UnityEditor.SceneManagement;
+using UnityEngine.Assertions;
+using UnityEditor.VersionControl;
 
 namespace CuttingRoom
 {
@@ -25,8 +29,26 @@ namespace CuttingRoom
         /// </summary>
         private SourceLocation sourceLocation = SourceLocation.VideoClip;
 
-        [SerializeField]
         public VideoClip Video = null;
+
+        [SerializeField]
+        private bool fullscreen = true;
+        private UIDocument uiDocument;
+
+        [SerializeField]
+        private int width = 1920;
+        [SerializeField]
+        private int height = 1080;
+        [SerializeField]
+        private int marginTopPercent = 0;
+        [SerializeField]
+        private int marginLeftPercent = 0;
+        [SerializeField]
+        private int marginTop = 0;
+        [SerializeField]
+        private int marginLeft = 0;
+
+        private VisualElement rootVisualElement = null;
 
         private VideoPlayer videoPlayer = null;
 
@@ -44,35 +66,80 @@ namespace CuttingRoom
                 {
                     videoPlayer = gameObject.AddComponent<VideoPlayer>();
                 }
-                // Get or Add a camera for rendering.
-                if (!gameObject.TryGetComponent(out videoPlayerCamera))
+
+                if (videoPlayer != null)
                 {
-                    videoPlayerCamera = gameObject.AddComponent<Camera>();
-                }
-
-                if (videoPlayer != null && videoPlayerCamera != null)
-                {
-                    videoPlayerCamera.clearFlags = CameraClearFlags.SolidColor;
-                    videoPlayerCamera.backgroundColor = Color.black;
-
-                    // Hide the camera.
-                    videoPlayerCamera.enabled = false;
-
                     videoPlayer.playOnAwake = false;
-
                     videoPlayer.aspectRatio = VideoAspectRatio.FitHorizontally;
 
-                    // We are rendering to the near plane (for now...)
-                    videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+                    if (fullscreen)
+                    {
+                        // We are rendering to the near plane (for now...)
+                        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+                        // Get or Add a camera for rendering.
+                        if (!gameObject.TryGetComponent(out videoPlayerCamera))
+                        {
+                            videoPlayerCamera = gameObject.AddComponent<Camera>();
+                        }
+                        if (videoPlayerCamera != null)
+                        {
+                            videoPlayerCamera.clearFlags = CameraClearFlags.SolidColor;
+                            videoPlayerCamera.backgroundColor = Color.black;
+
+                            // Hide the camera.
+                            videoPlayerCamera.enabled = false;
+
+                            // Set the render depth of the camera. This comes from the layer that this video controller is sequenced onto.
+                            //videoPlayer.targetCamera.depth = renderDepth - 1;
+                            videoPlayer.targetCamera = videoPlayerCamera;
+                        }
+                    }
+                    else
+                    {
+                        videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+
+                        RenderTexture videoRenderTex = new RenderTexture(width, height, 0);
+                        videoPlayer.targetTexture = videoRenderTex;
+
+                        if (!gameObject.TryGetComponent(out uiDocument))
+                        {
+                            uiDocument = gameObject.AddComponent<UIDocument>();
+                        }
+
+                        if (uiDocument != null)
+                        {
+                            if (uiDocument.panelSettings == null)
+                            {
+                                uiDocument.panelSettings = Resources.Load<PanelSettings>("CuttingRoom/UI/UIVideoPanelSettings");
+                            }
+                            rootVisualElement = uiDocument.rootVisualElement;
+
+                            rootVisualElement.style.width = Screen.width;
+                            rootVisualElement.style.height = Screen.height;
+                            rootVisualElement.style.flexDirection = FlexDirection.Row;
+                            rootVisualElement.style.alignContent = Align.Center;
+
+
+                            VisualElement videoImage = new VisualElement();
+
+                            videoImage.style.backgroundImage = new Background() { renderTexture = videoRenderTex } ;
+
+                            videoImage.style.width = width;
+                            videoImage.style.height = height;
+                            videoImage.style.position = Position.Relative;
+                            videoImage.style.alignSelf = Align.Center;
+                            videoImage.style.marginTop = marginTop; // Screen.height * ((float)marginTopPercent / 100);
+                            videoImage.style.marginLeft = marginLeft; // Screen.width * ((float)marginLeftPercent / 100);
+
+                            rootVisualElement.Add(videoImage);
+                        }
+                    }
 
                     videoPlayer.Pause();
 
                     // Preload the video player content.
                     videoPlayer.Prepare();
 
-                    // Set the render depth of the camera. This comes from the layer that this video controller is sequenced onto.
-                    //videoPlayer.targetCamera.depth = renderDepth - 1;
-                    videoPlayer.targetCamera = videoPlayerCamera;
 
                     Initialised = true;
                 }
@@ -89,7 +156,10 @@ namespace CuttingRoom
             {
                 videoPlayer.clip = Video;
 
-                videoPlayerCamera.enabled = true;
+                if (videoPlayerCamera != null)
+                {
+                    videoPlayerCamera.enabled = true;
+                }
 
                 videoPlayer.loopPointReached += (player) => { contentEnded = true; };
 
@@ -118,6 +188,10 @@ namespace CuttingRoom
             Debug.Log("Destroying video player: " + gameObject.name);
 
             Destroy(videoPlayer);
+            if (uiDocument != null)
+            {
+                Destroy(uiDocument);
+            }
         }
 
         public override IEnumerator WaitForEndOfContent()
