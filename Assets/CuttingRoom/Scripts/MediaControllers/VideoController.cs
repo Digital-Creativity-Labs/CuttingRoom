35e2bde6ca61fe8f9f5e50b543e21209f7817ec6
Daniel Slawson <daniel.slawson@york.ac.uk>
2023-05-22 11:57:15 +0100

    WIP: start adding support for using skybox for immersive videow

--- Assets/CuttingRoom/Scripts/MediaControllers/VideoController.cs
+++ Assets/CuttingRoom/Scripts/MediaControllers/VideoController.cs
@@ -22,6 +22,14 @@ namespace CuttingRoom
             //StreamingAssets
         }
 
+        public enum RenderType
+        {
+            Fullscreen,
+            PIP,
+            Immersive360,
+            Immersive180
+        }
+
         /// <summary>
         /// Where the video clip source is loaded from for this controller.
         /// </summary>
@@ -30,6 +38,8 @@ namespace CuttingRoom
         public VideoClip Video = null;
 
         public bool fullscreen = true;
+
+        public RenderType renderType = RenderType.Fullscreen;
 
         public string url = string.Empty;
 
@@ -63,7 +73,20 @@ namespace CuttingRoom
                 videoPlayer.playOnAwake = false;
                 videoPlayer.aspectRatio = VideoAspectRatio.FitHorizontally;
 
-                if (fullscreen)
+                if (sourceLocation == SourceLocation.VideoClip && Video != null)
+                {
+                    videoPlayer.clip = Video;
+                }
+                else if (sourceLocation == SourceLocation.Url && !string.IsNullOrEmpty(url))
+                {
+                    videoPlayer.url = url;
+                }
+                else
+                {
+                    // Not supported or not configured correctly
+                }
+
+                if (fullscreen && renderType == RenderType.Fullscreen)
                 {
                     // We are rendering to the near plane (for now...)
                     videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
@@ -92,7 +115,7 @@ namespace CuttingRoom
                         videoPlayer.targetCamera = videoPlayerCamera;
                     }
                 }
-                else
+                else if (renderType == RenderType.PIP)
                 {
                     videoPlayer.renderMode = VideoRenderMode.RenderTexture;
 
@@ -128,6 +151,26 @@ namespace CuttingRoom
 
                         rootVisualElement.Add(videoImage);
                     }
+                }
+                else if (renderType == RenderType.Immersive360 || renderType == RenderType.Immersive180)
+                {
+                    videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+
+                    RenderTexture videoRenderTex = new RenderTexture((int)videoPlayer.width, (int)videoPlayer.height, 0);
+                    videoPlayer.targetTexture = videoRenderTex;
+
+                    Material skyboxMaterial;
+                    if (renderType == RenderType.Immersive360)
+                    {
+                        skyboxMaterial = Resources.Load<Material>("CuttingRoom/Render/Immersive/Skybox360");
+                    }
+                    else
+                    {
+                        skyboxMaterial = Resources.Load<Material>("CuttingRoom/Render/Immersive/Skybox180");
+                    }
+                    skyboxMaterial.mainTexture = videoRenderTex;
+
+                    RenderSettings.skybox = skyboxMaterial;
                 }
 
                 if (sourceLocation == SourceLocation.VideoClip && Video != null)
